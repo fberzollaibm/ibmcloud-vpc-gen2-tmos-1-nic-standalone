e0e623f21860cc6832ff9dacfc60cb8bdf9458b0
Sakthi <sakshan1@in.ibm.com>
2020-06-10 22:17:50 +0530

    image copy related code changes
    Copy image from public COS bucket to custom image

---
+++ image.tf
@@ -0,0 +1,40 @@
+
+locals {
+  image_url = "cos://${var.region}/${var.vnf_bucket_base_name}-${var.region}/${var.vnf_cos_image_name}"
+}
+
+# Generating random ID
+resource "random_uuid" "test" {}
+
+resource "ibm_is_image" "f5_custom_image" {
+  depends_on       = ["random_uuid.test"]
+  href             = "${local.image_url}"
+  name             = "${var.vnf_vpc_image_name}-${substr(random_uuid.test.result, 0, 8)}"
+  operating_system = "centos-7-amd64"
+  resource_group   = "${data.ibm_resource_group.rg.id}"
+
+  timeouts {
+    create = "30m"
+    delete = "10m"
+  }
+}
+
+data "ibm_is_image" "f5_custom_image" {
+  name       = "${var.vnf_vpc_image_name}-${substr(random_uuid.test.result, 0, 8)}"
+  depends_on = ["ibm_is_image.f5_custom_image"]
+}
+
+# Delete custom image from the local user after VSI creation.
+data "external" "delete_custom_image" {
+  depends_on = ["ibm_is_instance.f5_vsi"]
+  program    = ["bash", "${path.module}/scripts/delete_custom_image.sh"]
+
+  query = {
+    custom_image_id = "${data.ibm_is_image.f5_custom_image.id}"
+    region          = "${var.region}"
+  }
+}
+
+output "delete_custom_image" {
+  value = "${lookup(data.external.delete_custom_image.result, "custom_image_id")}"
+}
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "ibm_is_image" "tmos_image" {
-    name = var.tmos_image_name
+  name = var.tmos_image_name
 }
 
 data "ibm_is_subnet" "f5_subnet" {
@@ -17,7 +17,7 @@ data "ibm_is_instance_profile" "instance_profile" {
 data "template_file" "user_data" {
   template = "${file("${path.module}/user_data.yaml")}"
   vars = {
-    tmos_admin_password = var.tmos_admin_password
+    tmos_admin_password  = var.tmos_admin_password
     tmos_license_basekey = var.tmos_license_basekey
   }
 }
@@ -85,17 +85,18 @@ resource "ibm_is_security_group_rule" "f5_tmm_out_icmp" {
 }
 
 resource "ibm_is_instance" "f5_ve_instance" {
-  name    = var.instance_name
-  image   = data.ibm_is_image.tmos_image.id
+  name = var.instance_name
+  #image   = data.ibm_is_image.tmos_image.id
+  image   = "${data.ibm_is_image.f5_custom_image.id}"
   profile = data.ibm_is_instance_profile.instance_profile.id
   primary_network_interface {
     name            = "tmm-1nic"
     subnet          = data.ibm_is_subnet.f5_subnet.id
     security_groups = [ibm_is_security_group.f5_tmm_sg.id]
   }
-  vpc  = data.ibm_is_subnet.f5_subnet.vpc
-  zone = data.ibm_is_subnet.f5_subnet.zone
-  keys = [data.ibm_is_ssh_key.f5_ssh_pub_key.id]
+  vpc       = data.ibm_is_subnet.f5_subnet.vpc
+  zone      = data.ibm_is_subnet.f5_subnet.zone
+  keys      = [data.ibm_is_ssh_key.f5_ssh_pub_key.id]
   user_data = data.template_file.user_data.rendered
 }
 
