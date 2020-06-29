ce12be12d263d6683f1b8770de2b4c6b5267c399
f5craigsca <c.scarborough@f5.com>
2020-06-29 14:19:34 -0400

    0.12 compatibility

--- image.tf
+++ image.tf
@@ -1,6 +1,6 @@
 data "ibm_resource_group" "rg" {
-  depends_on = ["data.ibm_is_subnet.f5_subnet"]
-  name = "${data.ibm_is_subnet.f5_subnet.resource_group_name}"
+  depends_on = [data.ibm_is_subnet.f5_subnet]
+  name       = data.ibm_is_subnet.f5_subnet.resource_group_name
 }
 
 locals {
@@ -8,14 +8,15 @@ locals {
 }
 
 # Generating random ID
-resource "random_uuid" "test" {}
+resource "random_uuid" "test" {
+}
 
 resource "ibm_is_image" "f5_custom_image" {
-  depends_on       = ["random_uuid.test"]
-  href             = "${local.image_url}"
+  depends_on       = [random_uuid.test]
+  href             = local.image_url
   name             = "${var.vnf_vpc_image_name}-${substr(random_uuid.test.result, 0, 8)}"
   operating_system = "centos-7-amd64"
-  resource_group   = "${data.ibm_resource_group.rg.id}"
+  resource_group   = data.ibm_resource_group.rg.id
 
   timeouts {
     create = "30m"
@@ -25,20 +26,21 @@ resource "ibm_is_image" "f5_custom_image" {
 
 data "ibm_is_image" "f5_custom_image" {
   name       = "${var.vnf_vpc_image_name}-${substr(random_uuid.test.result, 0, 8)}"
-  depends_on = ["ibm_is_image.f5_custom_image"]
+  depends_on = [ibm_is_image.f5_custom_image]
 }
 
 # Delete custom image from the local user after VSI creation.
 data "external" "delete_custom_image" {
-  depends_on = ["ibm_is_instance.f5_ve_instance"]
+  depends_on = [ibm_is_instance.f5_ve_instance]
   program    = ["bash", "${path.module}/scripts/delete_custom_image.sh"]
 
   query = {
-    custom_image_id = "${data.ibm_is_image.f5_custom_image.id}"
-    region          = "${var.region}"
+    custom_image_id = data.ibm_is_image.f5_custom_image.id
+    region          = var.region
   }
 }
 
 output "delete_custom_image" {
-  value = "${lookup(data.external.delete_custom_image.result, "custom_image_id")}"
+  value = data.external.delete_custom_image.result["custom_image_id"]
 }
+
